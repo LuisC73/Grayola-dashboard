fb78d205d127228667748053a79896267a0c002f
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2024-11-17 13:06:57 -0500

    Feat: Added public routes to the middleware and redirected to the dashboard if the user is logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,23 @@
 import { NextResponse, type NextRequest } from 'next/server';
 
 const protectedRoutes = ['/dashboard', '/dashboard/projects'];
+const publicRoutes = ['/login', '/register'];
 
 export async function middleware(req: NextRequest) {
   const cookieStore = req.cookies;
   const token = cookieStore.get('supabase-auth-token');
 
-  if (protectedRoutes.includes(req.nextUrl.pathname)) {
+  const { pathname } = req.nextUrl;
+
+  if (protectedRoutes.includes(pathname)) {
     if (!token) {
       return NextResponse.redirect(new URL('/', req.url));
+    }
+  }
+
+  if (publicRoutes.includes(pathname)) {
+    if (token) {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
     }
   }
 
