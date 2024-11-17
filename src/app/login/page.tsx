9351047ad3ce150a270f7286b8646265ced61d71
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2024-11-16 22:20:34 -0500

    Fix: improved responsiveness on login and register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -48,7 +48,7 @@ export default function LoginPage() {
 
   return (
     <div className="grid grid-cols-1 grid-rows-1 min-h-screen justify-items-center">
-      <main className="px-10 py-5 grid grid-cols-1 lg:grid-cols-2 items-center lg:justify-items-center gap-10 max-w-[1400px]">
+      <main className="px-5 md:px-10 py-5 grid grid-cols-1 lg:grid-cols-2 items-center lg:justify-items-center gap-10 max-w-[1400px]">
         <div className="flex flex-col gap-5 lg:w-4/5">
           <Image src={LOGIN_CONTENT.logo.src} alt={LOGIN_CONTENT.logo.alt} width={LOGIN_CONTENT.logo.width} height={LOGIN_CONTENT.logo.height} />
           <div className="flex flex-col gap-2 border-b border-gray-300 pb-5">
@@ -65,7 +65,7 @@ export default function LoginPage() {
             {error && <p style={{ color: 'red' }}>{error}</p>}
           </form>
         </div>
-        <div className="w-full h-full min-h-[470px] max-h-[570px] p-10 bg-tertiary bg-decorate bg-no-repeat bg-right-bottom  bg-[length:85%_auto] md:bg-[length:45%_auto] lg:bg-[length:65%_auto] rounded-2xl overflow-hidden">
+        <div className="w-full h-full min-h-[470px] max-h-[600px] p-10 bg-tertiary bg-decorate bg-no-repeat bg-right-bottom  bg-[length:85%_auto] md:bg-[length:45%_auto] lg:bg-[length:65%_auto] rounded-2xl overflow-hidden">
           <div className="flex flex-col gap-5 w-4/5">
             <h2 className="font-[family-name:var(--font-title)] text-2xl">{LOGIN_CONTENT.subtitle}</h2>
             <Link href={LOGIN_CONTENT.button.href} title={LOGIN_CONTENT.button.title} className="text-sm w-fit font-[family-name:var(--font-body)] hover:underline">
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -59,8 +59,8 @@ export default function RegisterPage() {
 
   return (
     <div className="grid grid-cols-1 grid-rows-1 min-h-screen justify-items-center">
-      <main className="px-10 py-5 grid grid-cols-1 lg:grid-cols-2 items-center lg:justify-items-center gap-10 max-w-[1400px]">
-        <div className="order-2 lg:order-1 w-full h-full min-h-[470px] max-h-[570px] p-10 bg-tertiary bg-decorate bg-no-repeat bg-right-bottom bg-[length:85%_auto] md:bg-[length:45%_auto] lg:bg-[length:65%_auto] rounded-2xl overflow-hidden">
+      <main className="px-5 md:px-10 py-5 grid grid-cols-1 lg:grid-cols-2 items-center lg:justify-items-center gap-10 max-w-[1400px]">
+        <div className="order-2 lg:order-1 w-full h-full min-h-[470px] max-h-[600px] p-10 bg-tertiary bg-decorate bg-no-repeat bg-right-bottom bg-[length:85%_auto] md:bg-[length:45%_auto] lg:bg-[length:65%_auto] rounded-2xl overflow-hidden">
           <div className="flex flex-col gap-5 w-4/5">
             <h2 className="font-[family-name:var(--font-title)] text-2xl">{REGISTER_CONTENT.subtitle}</h2>
             <Link href={REGISTER_CONTENT.button.href} title={LOGIN_CONTENT.button.title} className="text-sm w-fit font-[family-name:var(--font-body)] hover:underline">
