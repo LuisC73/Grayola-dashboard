5c2a1171fe3d143d722d40ea792e7ddcfec88cd9
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2024-11-18 17:33:39 -0500

    Feat: Temporarily implemented URLSearchParams to fix error on the edit page

--- src/app/dashboard/projects/edit/page.tsx
+++ src/app/dashboard/projects/edit/page.tsx
@@ -5,9 +5,8 @@ import { editProject } from '@/services/editProject';
 import { getDesigners } from '@/services/getDesigners';
 import { DesignerProps } from '@types';
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { getProjectById } from '@/services/getProjectById';
-import { Suspense } from 'react';
 
 export default function EditProjectPage() {
   const [projectData, setProjectData] = useState({
@@ -19,9 +18,16 @@ export default function EditProjectPage() {
   const [success, setSuccess] = useState<boolean>(false);
   const [options, setOptions] = useState<DesignerProps[]>([]);
   const [isModalActive, setIsModalActive] = useState<boolean>(false);
-  const searchParams = useSearchParams();
-  const projectId = searchParams.get('id') as string;
+  const [projectId, setProjectId] = useState<string>('');
   const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const searchParams = new URLSearchParams(window.location.search);
+      const id = searchParams.get('id');
+      setProjectId(id as string);
+    }
+  }, []);
 
   useEffect(() => {
     const fetchProjectData = async () => {
@@ -89,61 +95,57 @@ export default function EditProjectPage() {
     router.push('/dashboard/projects');
   };
 
+  if (!projectId) {
+    <p className="font-[family-name:var(--font-body)] text-sm text-gray-900">Cargando datos...</p>;
+  }
+
   return (
-    <Suspense
-      fallback={
-        <p className="font-[family-name:var(--font-body)] text-sm text-gray-900">
-          Cargando datos...
-        </p>
-      }
-    >
-      <div className="w-full grid grid-rows-[auto_1fr]">
-        <div className="p-5 border-b border-gray-300 grid grid-rows-1 grid-cols-[1fr_auto] items-center">
-          <h1 className="font-[family-name:var(--font-title)] text-black text-base">
-            Editar Proyecto
-          </h1>
-          <ButtonLink
-            label="Volver"
-            style="Primary"
-            href="/dashboard/projects"
-            title="Volver a proyectos"
-          />
-        </div>
-        <div className="p-5 grid grid-cols-1 md:grid-cols-2 gap-5">
-          <div className="order-2 md:order-1 w-full h-full min-h-[320px] md:min-h-full max-h-[500px] p-5 lg:p-10 bg-tertiary bg-planet bg-no-repeat bg-bottom bg-[length:65%_auto] rounded-md overflow-hidden">
-            <h2 className="font-[family-name:var(--font-title)] text-2xl">
-              Administra tus proyectos de forma rápida y eficiente.
-            </h2>
-          </div>
-          <div className="order-1 md:order-2">
-            <EditProjectForm
-              data={projectData}
-              onSubmit={handleOnSubmit}
-              changeTitle={handleChangeTitle}
-              changeDescription={handleChangeDescription}
-              changeDesigner={handleChangeDesigner}
-              options={options}
-              errorMsg={error}
-            />
-          </div>
-        </div>
-        {error && isModalActive && (
-          <AlertModal
-            type="Error"
-            title="Ha ocurrido un error"
-            description={error}
-            onClose={handleCloseModal}
-          />
-        )}
-        {success && isModalActive && (
-          <AlertModal
-            type="Success"
-            title="Proyecto editado"
-            description="El proyecto se ha editado con éxito."
-            onClose={handleCloseModal}
-          />
-        )}
+    <div className="w-full grid grid-rows-[auto_1fr]">
+      <div className="p-5 border-b border-gray-300 grid grid-rows-1 grid-cols-[1fr_auto] items-center">
+        <h1 className="font-[family-name:var(--font-title)] text-black text-base">
+          Editar Proyecto
+        </h1>
+        <ButtonLink
+          label="Volver"
+          style="Primary"
+          href="/dashboard/projects"
+          title="Volver a proyectos"
+        />
       </div>
-    </Suspense>
+      <div className="p-5 grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="order-2 md:order-1 w-full h-full min-h-[320px] md:min-h-full max-h-[500px] p-5 lg:p-10 bg-tertiary bg-planet bg-no-repeat bg-bottom bg-[length:65%_auto] rounded-md overflow-hidden">
+          <h2 className="font-[family-name:var(--font-title)] text-2xl">
+            Administra tus proyectos de forma rápida y eficiente.
+          </h2>
+        </div>
+        <div className="order-1 md:order-2">
+          <EditProjectForm
+            data={projectData}
+            onSubmit={handleOnSubmit}
+            changeTitle={handleChangeTitle}
+            changeDescription={handleChangeDescription}
+            changeDesigner={handleChangeDesigner}
+            options={options}
+            errorMsg={error}
+          />
+        </div>
+      </div>
+      {error && isModalActive && (
+        <AlertModal
+          type="Error"
+          title="Ha ocurrido un error"
+          description={error}
+          onClose={handleCloseModal}
+        />
+      )}
+      {success && isModalActive && (
+        <AlertModal
+          type="Success"
+          title="Proyecto editado"
+          description="El proyecto se ha editado con éxito."
+          onClose={handleCloseModal}
+        />
+      )}
+    </div>
   );
 }
