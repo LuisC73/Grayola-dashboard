809ed6c865f404e0211c9e183cf1b1953974bebd
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2024-11-18 17:16:09 -0500

    Feat: Fixed incorrect property in the select

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -5,7 +5,7 @@ import { SelectProps } from '@types';
 export function Select({
   id,
   label,
-  initialOption,
+  initialValue,
   options,
   isRequired,
   parentMethod,
@@ -21,7 +21,7 @@ export function Select({
       <select
         id={id}
         onChange={(e) => parentMethod && parentMethod(e)}
-        defaultValue={initialOption}
+        defaultValue={initialValue}
         required={isRequired}
         className="text-gray-900 font-[family-name:var(--font-body)] text-sm bg-gray-100 border border-gray-300 p-2 rounded-md"
       >
