dc2cde645c07c3340eec2bd4b68f5715ff3cb2f9
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2024-11-19 11:59:15 -0500

    Fix: Improved cookie handling function for better efficiency and security.

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,3 +1,9 @@
-export const saveTokenToCookie = (token: string) => {
-  document.cookie = `supabase-auth-token=${token}; path=/; max-age=${60 * 60 * 24 * 7}; secure; SameSite=Strict`;
+"use server";
+
+import { cookies } from 'next/headers';
+
+export const saveTokenToCookie =  async (token: string) => {
+  const cookieStore = await cookies()
+  cookieStore.set('supabase-auth-token', token);
+  // document.cookie = `supabase-auth-token=${token}; path=/; max-age=${60 * 60 * 24 * 7}; secure; SameSite=Strict`;
 };
